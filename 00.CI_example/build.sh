64579a044c5b935f60d531c0028457b2a40e8cec
Dmitry V. Luciv <dluciv@dluciv.name>
2025-03-12 16:22:44 +0300

    Again

--- 00.CI_example/build.sh
+++ 00.CI_example/build.sh
@@ -6,9 +6,9 @@ rm example_test example *.o 2>/dev/null
 
 set -e
 
-COMMONFLAGS="-Werror -Wpedantic -Wall -std=c++17"
-CCFLAGS="${COMMONFLAGS} $(pkgconf --cflags gtest)"
-LDFLAGS="${COMMONFLAGS} $(pkgconf --libs gtest) -pthread"
+COMMONFLAGS="-std=c++17"
+CCFLAGS="${COMMONFLAGS} -Werror -Wpedantic -Wall $(pkgconf --cflags gtest)"
+LDFLAGS="${COMMONFLAGS} $(pkgconf --libs gtest) -lgtest_main -lpthread"
 
 echo "CCFLAGS: $CCFLAGS"
 echo "LDFLAGS: $LDFLAGS"
@@ -18,4 +18,4 @@ g++ $CCFLAGS -c example.cc
 g++ $CCFLAGS -c lib_test.cc
 
 g++ $LDFLAGS -o example example.o lib.o
-g++ $LDFLAGS -lgtest_main -o example_test lib_test.o lib.o
+g++ $LDFLAGS -o example_test lib_test.o lib.o
