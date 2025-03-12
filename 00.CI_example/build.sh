1089aff8451cb83f44a403b3d3329ee75163e627
Dmitry V. Luciv <dluciv@dluciv.name>
2025-03-12 16:24:53 +0300

    Again

--- 00.CI_example/build.sh
+++ 00.CI_example/build.sh
@@ -14,8 +14,8 @@ echo "CCFLAGS: $CCFLAGS"
 echo "LDFLAGS: $LDFLAGS"
 
 g++ $CCFLAGS -c lib.cc
-g++ $CCFLAGS -c example.cc
+# g++ $CCFLAGS -c example.cc
 g++ $CCFLAGS -c lib_test.cc
 
-g++ $LDFLAGS -o example example.o lib.o
+# g++ -o example example.o lib.o
 g++ $LDFLAGS -o example_test lib_test.o lib.o
